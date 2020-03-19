291f463b2f92efe75da2e1840fde86236f676ca6
Venura Athukorala <venura9@users.noreply.github.com>
2020-03-20 00:45:08 +1100

    debug 7

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,16 @@
 _client_id=$(echo $6 | jq -r '.clientId')
 _client_secret=$(echo $6 | jq -r '.clientSecret')
 _tenant_id=$(echo $6 | jq -r '.tenantId')
+_subscription_id=$(echo $6 | jq -r '.subscriptionId')
 
+# Login to azure using service principal
 az login --service-principal -u $_client_id -p $_client_secret --tenant $_tenant_id
 
-#az group list
+# Select the subscription
+az account set --subscription $_subscription_id
 
-echo _client_id: $_client_id
-echo _tenant_id: ***********
-echo _tenant_id: $_tenant_id
+# Sample list of resources
+az group list
 
 time=$(date)
 dig +short myip.opendns.com @resolver1.opendns.com
