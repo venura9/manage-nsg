3761d93951e7bdc0c30e00928e7f2fcc114dc5ff
Venura Athukorala <vathukorala@gmail.com>
2020-03-20 01:05:10 +1100

    updated to look proper

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,17 @@
 #!/bin/sh -l
 
-_client_id=$(echo $6 | jq -r '.clientId')
-_client_secret=$(echo $6 | jq -r '.clientSecret')
-_tenant_id=$(echo $6 | jq -r '.tenantId')
-_subscription_id=$(echo $6 | jq -r '.subscriptionId')
+_client_id=$(echo $5 | jq -r '.clientId')
+_client_secret=$(echo $5 | jq -r '.clientSecret')
+_tenant_id=$(echo $5 | jq -r '.tenantId')
+_subscription_id=$(echo $5 | jq -r '.subscriptionId')
+_public_ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
+
+_rule_priority_start=$1
+_rule_priority_end=$1+$2
+_rule_port=$3
+
+_action=$4
+
 
 # Login to azure using service principal
 az login --service-principal -u $_client_id -p $_client_secret --tenant $_tenant_id
@@ -11,9 +19,11 @@ az login --service-principal -u $_client_id -p $_client_secret --tenant $_tenant
 # Select the subscription
 az account set --subscription $_subscription_id
 
-# Sample list of resources
-# az group list
+echo _action: $_action
+echo _rule_port: $_rule_port
+echo _rule_priority_start: $_rule_priority_start
+echo _rule_priority_end: $_rule_priority_end
 
-time=$(date)
-dig +short myip.opendns.com @resolver1.opendns.com
-echo ::set-output name=time::$time
+
+#time=$(date)
+#echo ::set-output name=time::$time
