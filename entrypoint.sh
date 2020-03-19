f1cfcabe22148e6d798bb0d053aa8d279e4e4756
Venura Athukorala <venura9@users.noreply.github.com>
2020-03-19 23:38:32 +1100

    debug 2

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,17 @@
 
 echo "Hello $1"
 
-#_client_id=$(echo $6 | jq '.clientId')
-#_client_secret=$(echo $6 | jq '.clientSecret')
-#_tenant_id=$(echo $6 | jq '.tenantId')
+_client_id=$(echo $6 | jq '.clientId')
+_client_secret=$(echo $6 | jq '.clientSecret')
+_tenant_id=$(echo $6 | jq '.tenantId')
 
 #az login --service-principal -u _client_id -p _client_secret --tenant _tenant_id
 
 #az group list
 
-echo $6 | jq '.clientId'
+echo _client_id
+echo _client_secret
+echo _tenant_id
 
 time=$(date)
 dig +short myip.opendns.com @resolver1.opendns.com
