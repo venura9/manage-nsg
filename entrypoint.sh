b932742fd1b676d9780cc5447c5e16a544337fa5
Venura Athukorala <venura9@users.noreply.github.com>
2020-03-19 23:46:41 +1100

    az login back on

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ _client_id=$(echo $6 | jq '.clientId')
 _client_secret=$(echo $6 | jq '.clientSecret')
 _tenant_id=$(echo $6 | jq '.tenantId')
 
-#az login --service-principal -u _client_id -p _client_secret --tenant _tenant_id
+az login --service-principal -u $_client_id -p $_client_secret --tenant $_tenant_id
 
 #az group list
 
