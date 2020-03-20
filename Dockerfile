a00a1bb35c867654b4d6fa18fbf25657e1e6f052
Venura Athukorala <vathukorala@gmail.com>
2020-03-20 13:00:23 +1100

    fixed a few things and moving for just add

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,9 @@
-# Container image that runs your code
 # FROM alpine:3.10
-# FROM mcr.microsoft.com/azure-cli:latest
 
+# https://hub.docker.com/_/microsoft-azure-cli
 # https://mcr.microsoft.com/v2/azure-cli/tags/list
+# FROM mcr.microsoft.com/azure-cli:latest
 FROM mcr.microsoft.com/azure-cli:2.2.0
-
 
 # Copies your code file from your action repository to the filesystem path `/` of the container
 COPY entrypoint.sh /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -33,18 +33,18 @@ then
 
     echo $_rule_name
 
-    # az network nsg rule create \
-    #     -g $_rule_nsg_resource_group \
-    #     --nsg-name $_rule_nsg \
-    #     -n $_rule_name \
-    #     --priority $_rule_priority \
-    #     --source-address-prefixes $_rule_public_ip/32 \
-    #     --source-port-ranges '*' \
-    #     --destination-address-prefixes '*' \
-    #     --destination-port-ranges '*' 
-    #     --access Allow \
-    #     --protocol Any \
-    #     --description "Allow from IP address $_rule_public_ip temporarily"
+    az network nsg rule create \
+        -g $_rule_nsg_resource_group \
+        --nsg-name $_rule_nsg \
+        -n $_rule_name \
+        --priority $_rule_priority \
+        --source-address-prefixes $_rule_public_ip/32 \
+        --source-port-ranges '*' \
+        --destination-address-prefixes '*' \
+        --destination-port-ranges '*' 
+        --access Allow \
+        --protocol Any \
+        --description "Allow from IP address $_rule_public_ip temporarily"
         
     echo ::set-output rule_name=$_rule_name
 else
