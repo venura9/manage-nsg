b3bb797f1a90d7032ec27c56bbd3fc80d1eabb89
Venura Athukorala <vathukorala@gmail.com>
2020-03-20 12:37:23 +1100

    debug 20

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,23 +31,25 @@ then
     _rule_priority=$(shuf -i $_rule_priority_start-$_rule_priority_end -n 1)
     _rule_name=$(manage-nsg-github-actions-$_rule_priority)
 
-    az network nsg rule create \
-        -g $_rule_nsg_resource_group \
-        --nsg-name $_rule_nsg \
-        -n $_rule_name \
-        --priority $_rule_priority \
-        --source-address-prefixes $_rule_public_ip/32 \
-        --source-port-ranges '*' \
-        --destination-address-prefixes '*' \
-        --destination-port-ranges '*' 
-        --access Allow \
-        --protocol Any \
-        --description "Allow from IP address $_rule_public_ip temporarily"
+    echo $_rule_name
+
+    # az network nsg rule create \
+    #     -g $_rule_nsg_resource_group \
+    #     --nsg-name $_rule_nsg \
+    #     -n $_rule_name \
+    #     --priority $_rule_priority \
+    #     --source-address-prefixes $_rule_public_ip/32 \
+    #     --source-port-ranges '*' \
+    #     --destination-address-prefixes '*' \
+    #     --destination-port-ranges '*' 
+    #     --access Allow \
+    #     --protocol Any \
+    #     --description "Allow from IP address $_rule_public_ip temporarily"
         
     echo ::set-output rule_name=$_rule_name
 else
     echo "Removing rule $_rule_id_for_removal"
-    az network nsg rule delete -g $_rule_nsg_resource_group --nsg-name $_rule_nsg -n $_rule_id_for_removal
+    # az network nsg rule delete -g $_rule_nsg_resource_group --nsg-name $_rule_nsg -n $_rule_id_for_removal
   
     echo ::set-output rule_name=$_rule_id_for_removal
 
