b57abffacee7457fca1cda9e2cc14308899e97f3
Venura Athukorala <venura9@users.noreply.github.com>
2020-03-20 13:47:32 +1100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,11 +32,11 @@ then
     _rule_name=manage-nsg-github-actions-$_rule_priority
 
     echo "Adding rule.... $_rule_name"
-    echo "az network nsg rule create -g $_rule_nsg_resource_group --nsg-name $_rule_nsg -n $_rule_name --priority $_rule_priority --source-address-prefixes $_rule_public_ip --source-port-ranges '*' --destination-address-prefixes '*' --destination-port-ranges '*' --access Allow --protocol Any --description 'Allow from IP address of github actions public runner temporarily'"
+    az network nsg rule create -g $_rule_nsg_resource_group --nsg-name $_rule_nsg -n $_rule_name --priority $_rule_priority --source-address-prefixes $_rule_public_ip --source-port-ranges '*' --destination-address-prefixes '*' --destination-port-ranges '*' --access Allow --protocol '*' --description 'Allow from IP address of github actions hosted runner temporarily'
     echo ::set-output rule_name=$_rule_name
 else
     echo "Removing rule $_rule_id_for_removal"
-    # az network nsg rule delete -g $_rule_nsg_resource_group --nsg-name $_rule_nsg -n $_rule_id_for_removal
+    #az network nsg rule delete -g $_rule_nsg_resource_group --nsg-name $_rule_nsg -n $_rule_id_for_removal
   
     echo ::set-output rule_name=$_rule_id_for_removal
 
