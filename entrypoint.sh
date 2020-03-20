c0ac2f0b3091c99620210b12a7184a5adb057fc3
Venura Athukorala <venura9@users.noreply.github.com>
2020-03-20 15:34:59 +1100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,29 +20,10 @@ az login --service-principal -u $_client_id -p $_client_secret --tenant $_tenant
 # Select the subscription
 az account set --subscription $_subscription_id
 
-# if [ -z "$_rule_id_for_removal" ]
-# then
-#     echo _rule_port: $_rule_port
-#     echo _rule_priority_start: $_rule_priority_start
-#     echo _rule_priority_end: $_rule_priority_end
-
-#     _rule_priority=$(shuf -i $_rule_priority_start-$_rule_priority_end -n 1)
-#     _rule_name=manage-nsg-github-actions-$_rule_priority
-
-#     echo "Adding rule.... $_rule_name"
-#     az network nsg rule create -g $_rule_nsg_resource_group --nsg-name $_rule_nsg -n $_rule_name --priority $_rule_priority --source-address-prefixes $_rule_public_ip --source-port-ranges '*' --destination-address-prefixes '*' --destination-port-ranges '*' --access Allow --protocol '*' --description 'Allow from IP address of github actions hosted runner temporarily'
-#     echo "::set-output rule_name=$_rule_name"
-# else
-#     echo "Removing rule $_rule_id_for_removal"
-#     #az network nsg rule delete -g $_rule_nsg_resource_group --nsg-name $_rule_nsg -n $_rule_id_for_removal
-#     echo "::set-output rule_name=$_rule_id_for_removal"
-# fi
-
-
 if [ $_rule_id_for_removal ]
 then
     echo "Removing rule $_rule_id_for_removal"
-    #az network nsg rule delete -g $_rule_nsg_resource_group --nsg-name $_rule_nsg -n $_rule_id_for_removal
+    az network nsg rule delete -g $_rule_nsg_resource_group --nsg-name $_rule_nsg -n $_rule_id_for_removal
     echo "::set-output name=rule_name::$_rule_id_for_removal"
 else
     echo _rule_port: $_rule_port
