0eb3d19a705a0c3aa98fdc41e58a99562fd1a7cf
Venura Athukorala <venura9@users.noreply.github.com>
2020-06-27 13:31:33 +1000

    Update entrypoint.sh
    comments updated to make the code readable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -l
 
+# Ensure the workflow fails on error
 set -e
 
 _azure_credentails=$1
@@ -9,8 +10,10 @@ _rule_port=$4
 _rule_id_for_removal=$5
 _rule_nsg_resource_group=$6
 _rule_nsg=$7
-_rule_public_ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
+_rule_public_ip=$(dig +short myip.opendns.com @resolver1.opendns.com) # Get runner's public IP
 
+
+# break the azure credientials JSON
 _client_id=$(echo $_azure_credentails | jq -r '.clientId')
 _client_secret=$(echo $_azure_credentails | jq -r '.clientSecret')
 _tenant_id=$(echo $_azure_credentails | jq -r '.tenantId')
@@ -24,10 +27,12 @@ az account set --subscription $_subscription_id
 
 if [ $_rule_id_for_removal ]
 then
+    # removing the rule
     echo "Removing rule $_rule_id_for_removal"
     az network nsg rule delete -g $_rule_nsg_resource_group --nsg-name $_rule_nsg -n $_rule_id_for_removal
     echo "::set-output name=rule_name::$_rule_id_for_removal"
 else
+    # adding the rule
     echo _rule_port: $_rule_port
     echo _rule_priority_start: $_rule_priority_start
     echo _rule_priority_end: $_rule_priority_end
@@ -42,5 +47,6 @@ else
 				--destination-address-prefixes '*' --destination-port-ranges '*' \
 				--access Allow --protocol '*' \
 				--description 'Allow from IP address of github actions hosted runner temporarily'
+    # output
     echo "::set-output name=rule_name::$_rule_name"
 fi
